236090eeabeb52d14f7698387c5855fd4a557def
Loic Delattre <loic.delattre@ecam.fr>
2025-03-21 08:12:12 +0100

    go toward ball

--- NodeForMovement.py
+++ NodeForMovement.py
@@ -26,13 +26,16 @@ class TurtlebotVisionController:
         """
         Callback function to process the received image and detect red objects.
         """
+        print("image sent")
         try:
             # Convert ROS Image message to OpenCV format
             cv_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding="bgr8")
 
             # Process the image to detect red color
             movement_cmd = self.process_image(cv_image)
+            print(movement_cmd)
 
+            print("sending data to robot")
             # Publish movement command
             self.publisher.publish(movement_cmd)
 
